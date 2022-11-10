ffc6e0170b15d99ea098b63957f7712fc1b46558
daya <dayajuangmahaputra@gmail.com>
2022-11-10 18:12:53 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,30 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create a scanner and a variable to track the loop and sum
+        Scanner scanner = new Scanner(System.in);
+        int count = 1;
+        int sum = 0;
+
+        while(count <= 10){
+            System.out.println("Enter number #" + count);
+
+            boolean hasNextInt = scanner.hasNextInt();
+            if(hasNextInt){
+                int num = scanner.nextInt();
+                sum += num;
+                count++;
+            }else{
+                System.out.println("Invalid Value");
+            }
+            scanner.nextLine();
+        }
+
+        System.out.println("The sum of your input is " + sum);
+
+        scanner.close();
     }
 }
